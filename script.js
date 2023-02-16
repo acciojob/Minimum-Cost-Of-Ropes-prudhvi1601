5dd6ad233c190b0d8969d95f1de336c8fa5e1e51
Prudhvi Raj <120363980+prudhvi1601@users.noreply.github.com>
2023-02-16 15:10:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 //your code here
-var input = document.querySelector("input").value;
+var input = document.querySelector("input");
 var result = document.getElementById("result");
 
-function minCost(a) {
-	a.preventDefault();
-	var arr = input.split(",");
+function minCost(event) {
+	event.preventDefault();
+	var arr = input.value.split(",");
 	arr.sort(function (a,b) {
 		return a-b;
 	});
+
+
 	var cost = 0;
 	while(arr.length>1){
-		var result = Number(arr[0]) + Number(arr[1]);
 		arr.splice(0,2);
 		arr.push(result);
-		cost += result;
+		cost += result.value;
 		arr.sort(function (a,b){
 			return a-b;
 		});
 	}
 	result.textContent = cost;
 }
-event.addEventListener("submit",minCost)
+input.addEventListener("submit",minCost)
 
 
 
